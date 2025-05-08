f418d67692207548dcb28fcaab04973c13c73393
narasimhaproj <lnreddyreddy405@gmail.com>
2025-05-08 21:53:36 +0530

    intial commit

---
+++ DockerSpringBootApp01/src/test/java/com/ln/DockerSpringBootApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootApp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
